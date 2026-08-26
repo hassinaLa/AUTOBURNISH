10265b1c6706ffd765c21f071ba102402750255d
Hassina <hassinalakehali123@gmail.com>
2026-08-26 21:23:39 +0100

    Initial website

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* ========================================
+   AUTO BURNISH
+   MAIN JAVASCRIPT
+======================================== */
+
+
+/* ---------- MOBILE MENU ---------- */
+
+const menuButton = document.querySelector(".menu-btn");
+
+menuButton.addEventListener("click", () => {
+
+    console.log("Mobile menu clicked");
+
+});
+
+
+/* ---------- LANGUAGE ---------- */
+
+const languageButton = document.querySelector(".language-btn");
+
+languageButton.addEventListener("click", () => {
+
+    console.log("Language selector clicked");
+
+});
